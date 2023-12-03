f3ed318ccecd600c55d2049e3b416c8d78f92710
kapb6901 <kap.bala@yahoo.com>
2023-12-03 16:50:20 -0500

    Button with frontend that performs transaction  between 2 accounts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [transactionStatus, setTransactionStatus] = useState(null);
+
+  const performTransaction = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/perform_transaction', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+      });
+
+      const result = await response.json();
+
+      if (result.success) {
+        setTransactionStatus('Transaction successful!');
+      } else {
+        setTransactionStatus(`Error: ${result.error}`);
+      }
+    } catch (error) {
+      console.error(error);
+      setTransactionStatus('An error occurred while performing the transaction.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>SimpleTransfer App</h1>
+      <h3>Press button to pay energy bill of 0.05 ether</h3>
+      <button onClick={performTransaction}>Perform Transaction</button>
+      {transactionStatus && <p>{transactionStatus}</p>}
     </div>
   );
 }
