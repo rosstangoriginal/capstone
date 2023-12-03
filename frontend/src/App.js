b82eee132d5d7e0060082453e9cefcc19d961fff
Ross Tang <ross.tang@ryerson.ca>
2023-12-02 22:45:28 -0500

    chart display incomplete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import Navbar from "./components/navigation/Navbar";
-import RoutePaths from "./components/navigation/RoutePaths"
+import RoutePaths from "./components/navigation/RoutePaths";
+import {useState} from 'react';
+import {UserData} from "./UserData";
+import LineChart from "./components/charts/LineChart";
 
-function App() {
+function App() { 
+    const [userData, setUserData] = useState({
+        labels: UserData.map((data) => data.month),
+        datasets: [{
+            label: "Energy Used (kWh)",
+            data: UserData.map((data) => data.usage)
+        }]
+    });
+    
     return (
         <div className="App">
             <Navbar />
             <h1>Blockchain Energy Project</h1>
+            <LineChart chartData={userData}/>
             <RoutePaths />
         </div>
     );
--- frontend/src/components/user-stats/EnergyUsage.jsx
+++ frontend/src/components/user-stats/EnergyUsage.jsx
@@ -1,11 +1,28 @@
+// import {useState} from 'react';
+// import {UserData} from "../../UserData";
+// // import {Line} from "react-chartjs-2";
+// import LineChart from "../charts/LineChart";
+// import {Chart as ChartJS} from 'chart.js/auto';
+
+
+
 export default (props) => {
-  return (
+    // const [userData, setUserData] = useState({
+    //     labels: UserData.map((data) => data.month),
+    //     datasets: [{
+    //         label: "Energy Used (kWh)",
+    //         data: UserData.map((data) => data.usage)
+    //     }]
+    // });
+
+    return (
     <div>
       <h1>Energy usage</h1>
       <p>
         {props.firstName}, your energy usage for {props.date} was{" "}
         {props.energyUsed} kWh
       </p>
+      {/* <LineChart chartData={userData}/> */}
     </div>
   );
 };
