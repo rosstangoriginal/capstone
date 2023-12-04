a0038640f52b0512026d72ac85ccd7c26e473127
rosstangoriginal <ross.tang@ryerson.ca>
2023-12-04 10:01:12 -0500

    componetize PayBill functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,40 @@
 import React, { useState } from 'react';
+import PayBill from './components/user-stats/PayBill';
 
 function App() {
-  const [transactionStatus, setTransactionStatus] = useState(null);
+//   const [transactionStatus, setTransactionStatus] = useState(null);
 
-  const performTransaction = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/perform_transaction', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({}),
-      });
+//   const performTransaction = async () => {
+//     try {
+//       const response = await fetch('http://localhost:3000/perform_transaction', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({}),
+//       });
 
-      const result = await response.json();
+//       const result = await response.json();
 
-      if (result.success) {
-        setTransactionStatus('Transaction successful!');
-      } else {
-        setTransactionStatus(`Error: ${result.error}`);
-      }
-    } catch (error) {
-      console.error(error);
-      setTransactionStatus('An error occurred while performing the transaction.');
-    }
-  };
+//       if (result.success) {
+//         setTransactionStatus('Transaction successful!');
+//       } else {
+//         setTransactionStatus(`Error: ${result.error}`);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       setTransactionStatus('An error occurred while performing the transaction.');
+//     }
+//   };
 
   return (
-    <div>
-      <h1>SimpleTransfer App</h1>
-      <h3>Press button to pay energy bill of 0.05 ether</h3>
-      <button onClick={performTransaction}>Perform Transaction</button>
-      {transactionStatus && <p>{transactionStatus}</p>}
-    </div>
+    <PayBill />
+    // <div>
+    //   <h1>SimpleTransfer App</h1>
+    //   <h3>Press button to pay energy bill of 0.05 ether</h3>
+    //   <button onClick={performTransaction}>Perform Transaction</button>
+    //   {transactionStatus && <p>{transactionStatus}</p>}
+    // </div>
   );
 }
 
---
+++ frontend/src/components/user-stats/PayBill.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const PayBill = () => {
+    const [transactionStatus, setTransactionStatus] = useState(null);
+
+    const performTransaction = async () => {
+        try {
+            const response = await fetch(
+                "http://localhost:3000/perform_transaction",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({}),
+                }
+            );
+
+            const result = await response.json();
+
+            if (result.success) {
+                setTransactionStatus("Transaction successful!");
+            } else {
+                setTransactionStatus(`Error: ${result.error}`);
+            }
+        } catch (error) {
+            console.error(error);
+            setTransactionStatus(
+                "An error occurred while performing the transaction."
+            );
+        }
+    };
+
+    return (
+        <div>
+            <h1>SimpleTransfer App</h1>
+            <h3>Press button to pay energy bill of 0.05 ether</h3>
+            <button onClick={performTransaction}>Perform Transaction</button>
+            {transactionStatus && <p>{transactionStatus}</p>}
+        </div>
+    );
+};
+
+export default PayBill;
