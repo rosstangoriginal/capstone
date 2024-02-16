36f4172f0c333e38169d11ca021583d7f012bb44
rosstangoriginal <ross.tang@ryerson.ca>
2024-02-16 10:12:36 -0500

    test push of new branch

--- frontend/src/components/user-authentication/LogIn.jsx
+++ frontend/src/components/user-authentication/LogIn.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import { getHashedPassword } from "./hash";
 
+//test push
 const Login = (props) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
